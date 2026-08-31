081d2a3bd389431ae25100d97e40f734a90ee43c
Tanvi Mhaskar <mhaskartanvi11@gmail.com>
2026-08-31 11:19:18 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+
+marks = [85, 78, 92, 88, 76]
+
+total = sum(marks)
+
+percentage = (total / 500) * 100
+
+average = total / 5
+
+print("=== Academic Performance ===")
+print("Marks:", marks)
+print("Total Marks:", total, "/ 500")
+print("Percentage:", percentage, "%")
+print("Average:", average)
